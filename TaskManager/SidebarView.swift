6fec0b713f6cf8850832d11422c561275d128c3e
Italo Gama <itrgama@gmail.com>
2024-12-22 19:58:01 -0300

    sidebar view

--- TaskManager/SidebarView.swift
+++ TaskManager/SidebarView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct SidebarView: View {
+    
+    let userCreatedGroups: [TaskGroup]
+    @State private var selection = TaskSection.all
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(selection: $selection) {
+            Section("Favorites") {
+                ForEach(TaskSection.allCases) { selection in
+                    Label(selection.displayName,
+                          systemImage: selection.iconName)
+                    .tag(selection)
+                }
+            }
+            
+            Section("Your Groups") {
+                ForEach(userCreatedGroups) { group in
+                    Label(group.title, systemImage: "folder")
+                        .tag(TaskSection.list(group))
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    SidebarView()
+    SidebarView(userCreatedGroups: TaskGroup.examples())
+        .listStyle(.sidebar)
 }
